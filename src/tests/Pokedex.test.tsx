f185c9c68ad9f8d76b1c77c5681c29ad047bee2b
cleitson <cleitson.ftw@gmail.com>
2023-08-11 16:04:29 -0300

    Testando o componente PokemonDetails completo

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -4,11 +4,16 @@ import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testando o componente Pokedex', () => {
-  test('Verificar as informações de determinado Pokémon', async () => {
+  test('Verificar as informações da pokedex', async () => {
     renderWithRouter(<App />);
+    const headingText = screen.getByRole('heading', { name: /encountered pokémon/i });
+    expect(headingText).toBeInTheDocument();
+
+    const btnNext = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(btnNext).toBeInTheDocument();
   });
 
-  test('Teste se existe um ícone de estrela nos Pokémon favoritados', async () => {
-    renderWithRouter(<App />, { route: '/pokemon/25' });
-  });
+  // test('Teste se existe um ícone de estrela nos Pokémon favoritados', async () => {
+  //   renderWithRouter(<App />, { route: '/pokemon/25' });
+  // });
 });
--- src/tests/PokemonDetails.test.tsx
+++ src/tests/PokemonDetails.test.tsx
@@ -1,1 +1,51 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const routePath = '/pokemon/65';
+
+describe('Testando o componente PokemonDetails', () => {
+  test('Teste se as informações detalhadas do Pokémon são mostradas na tela', () => {
+    renderWithRouter(<App />, { route: routePath });
+
+    const name = screen.getByRole('heading', { name: /alakazam details/i });
+    expect(name).toBeInTheDocument();
+    expect(screen.queryByText(/more details/i)).not.toBeInTheDocument();
+
+    const summary = screen.getByRole('heading', { level: 2, name: /summary/i });
+    expect(summary).toBeInTheDocument();
+
+    const paragraph = screen.getByText(/Closing both its eyes heightens/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  test('Existe na página uma seção com os mapas contendo as localizações do Pokémon', () => {
+    renderWithRouter(<App />, { route: routePath });
+
+    const headingName = screen.getByRole('heading', { level: 2, name: /game locations of alakazam/i });
+    expect(headingName).toBeInTheDocument();
+
+    const mapName = screen.getByText(/unova accumula town/i);
+    const imgMap = screen.getByRole('img', { name: /alakazam location/i });
+    const imgMapSrc = imgMap.getAttribute('src');
+    const imgMapAlt = imgMap.getAttribute('alt');
+
+    expect(mapName).toBeInTheDocument();
+    expect(imgMap).toBeInTheDocument();
+    expect(imgMapSrc).toBe('https://archives.bulbagarden.net/media/upload/4/44/Unova_Accumula_Town_Map.png');
+    expect(imgMapAlt).toBe('Alakazam location');
+  });
+
+  test('Teste se o usuário pode favoritar um Pokémon por meio da página de detalhes', async () => {
+    renderWithRouter(<App />, { route: routePath });
+
+    const favPokemon = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    expect(favPokemon).toBeInTheDocument();
+    screen.getByRole('checkbox', { checked: false });
+    await userEvent.click(favPokemon);
+    screen.getByRole('checkbox', { checked: true });
+    await userEvent.click(favPokemon);
+    screen.getByRole('checkbox', { checked: false });
+  });
+});
