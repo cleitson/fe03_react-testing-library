b5cbd6aa4f93e29a62bf0bc956f27655d0b31def
cleitson <cleitson.ftw@gmail.com>
2023-08-10 21:18:13 -0300

    Testando o componente NotFound completo

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componete NotFound', () => {
+  test('Teste se a pagina contem o texto, Page requested not found ', () => {
+    renderWithRouter(<App />, { route: '/dasdqweq' });
+
+    const verifyText = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(verifyText).toBeInTheDocument();
+
+    const verifyImgLink = screen.getByRole('img');
+    const imgUrl = verifyImgLink.getAttribute('alt');
+
+    expect(imgUrl).toBe('Clefairy pushing buttons randomly with text I have no idea what i\'m doing');
+  });
+});
